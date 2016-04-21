10179cd4e95746176f20e3a94b3294548d3bf4d6
Kyle Kwon <kyleskwon@gmail.com>
2016-04-20 23:28:38 -0700

    start cp5 assignment

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,21 +7,22 @@
             });
         
         $stateProvider
-        .state('landing', {
-            url: '/',
-            controller: 'LandingCtrl as landing',
-            templateUrl: '/templates/landing.html'
-        })
-        .state('album', {
-            url: '/album',
-            templateUrl: '/templates/album.html'
-        })
-        .state('collection', {
-            url: '/collection',
-            controller: 'CollectionCtrl as collection',
-            templateUrl: '/templates/collection.html'
-        });
-    }
+            .state('landing', {
+                url: '/',
+                controller: 'LandingCtrl as landing',
+                templateUrl: '/templates/landing.html'
+            })
+            .state('album', {
+                url: '/album',
+                controller: '/AlbumCtrl as album',
+                templateUrl: '/templates/album.html'
+            })
+            .state('collection', {
+                url: '/collection',
+                controller: 'CollectionCtrl as collection',
+                templateUrl: '/templates/collection.html'
+            });
+        }
     
     angular
         .module('blocJams', ['ui.router'])
