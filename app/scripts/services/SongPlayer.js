f78b66a7b7ffac2c0ec5f99982679b07eb2a5888
Kyle Kwon <kyleskwon@gmail.com>
2016-04-24 19:38:34 -0700

    add play and pause methods for album view

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -2,13 +2,35 @@
     function SongPlayer() {
         var SongPlayer = {};
         
+        var currentSong = null;
+        var currentBuzzObject = null;
+        
         SongPlayer.play = function(song) {
-            var currentBuzzObject = new buzz.sound(song.audioUrl, {
-                formats: ['mp3'],
-                preload: true
-            });
+            if (currentSong !== song) {
+                if (currentBuzzObject) {
+                    currentBuzzObject.stop();
+                    currentSong.playing = null;
+                } else if (currentSong === song) {
+                    if (currentBuzzObject.isPaused()) {
+                        currentBuzzObject.play();
+                    }
+                }
             
-            currentBuzzObject.play();
+                currentBuzzObject = new buzz.sound(song.audioUrl, {
+                    formats: ['mp3'],
+                    preload: true
+                });
+                
+                currentSong = song;
+            
+                currentBuzzObject.play();
+                song.playing = true;
+            }
+        };
+        
+        SongPlayer.pause = function(song) {
+            currentBuzzObject.pause();
+            song.playing = false;
         };
         
         return SongPlayer;
