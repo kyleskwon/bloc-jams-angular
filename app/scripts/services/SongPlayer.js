99ec5bab9f3d7d711413578fc06738f120e2bfc2
Kyle Kwon <kyleskwon@gmail.com>
2016-04-24 20:39:03 -0700

    refactor songplayer and add documentation for attributes functions and methods

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -2,32 +2,43 @@
     function SongPlayer() {
         var SongPlayer = {};
         
+        /** private attribute */
         var currentSong = null;
+        /** private attribute
+        * @desc Buzz object audio file
+        * @type {Object}
+        */
         var currentBuzzObject = null;
         
+        /** private function
+        * @function setSong
+        * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+        * @param {Object} song
+        */      
+        var setSong = function(song) {
+            if (currentBuzzObject) {
+                currentBuzzObject.stop();
+                currentSong.playing = null;
+            }
+            
+            currentBuzzObject = new buzz.sound(song.audioUrl, {
+                formats: ['mp3'],
+                preload: true
+            });
+            
+            currentSong = song;
+        };
+        
+        /** public method */
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
-                if (currentBuzzObject) {
-                    currentBuzzObject.stop();
-                    currentSong.playing = null;
-                } else if (currentSong === song) {
-                    if (currentBuzzObject.isPaused()) {
-                        currentBuzzObject.play();
-                    }
-                }
-            
-                currentBuzzObject = new buzz.sound(song.audioUrl, {
-                    formats: ['mp3'],
-                    preload: true
-                });
-                
-                currentSong = song;
-            
+                setSong(song);
                 currentBuzzObject.play();
                 song.playing = true;
             }
         };
         
+        /** public method */
         SongPlayer.pause = function(song) {
             currentBuzzObject.pause();
             song.playing = false;
