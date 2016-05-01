0c40251e4623b532989366b37b07a80f69a88657
Kyle Kwon <kyleskwon@gmail.com>
2016-05-01 12:49:45 -0700

    remove console.log outputs

--- dist/scripts/controllers/AlbumCtrl.js
+++ dist/scripts/controllers/AlbumCtrl.js
@@ -2,8 +2,6 @@
     function AlbumCtrl(Fixtures, SongPlayer) {
         this.albumData = Fixtures.getAlbum();
         this.songPlayer = SongPlayer;
-        console.log(this.albumData);
-        console.log(this.songPlayer);
     }
     
     angular
